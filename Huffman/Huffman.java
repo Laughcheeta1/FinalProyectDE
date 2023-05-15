41e74b7ff20bc85e21faed4eb4a272d369b3884c
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-05-14 21:17:04 -0500

    Added comments to the Huffman.java class, and added a new method that we are going to use, called 'countCharacters'

--- Huffman/Huffman.java
+++ Huffman/Huffman.java
@@ -1,14 +1,41 @@
 package Huffman;
 
+import java.util.HashMap;
+
 import java.io.File;
 
+/**
+ * Santiago Yepes Mesa, Simon Eduardo Parisca Monuñoz, Santiago Augusto Toro Bonilla
+ */
+
 public class Huffman {
+    /**
+     * Given a .txt file, returns the compressed version of this file in the way of a CompressedFile class object.
+     * @param file to compress
+     * @return CompressedFile object.
+     */
     public CompressedFile compressTxt(File file)
     {
 
     }
 
+    /**
+     * Given a compressed file, of the extension .compr, returns the .txt uncompressed version of this file
+     * @param compressedFile
+     * @return .txt File
+     */
     public File decompressFile(CompressedFile compressedFile)
+    {
+
+    }
+
+    /**
+     * Given a file returns the count of the characters in form of a hashmap, where its key is the
+     * character, and the value is the frequency of the character.
+     * @param file to count the number of characters
+     * @return HashMap that contains the number of chars
+     */
+    private HashMap<Character, Integer> countCharacters(File file)
     {
 
     }
