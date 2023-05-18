3824f1890fe7c050077be98ef8c95e7ca8ddeca8
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-05-18 11:14:05 -0500

    Minor change

--- Huffman/Huffman.java
+++ Huffman/Huffman.java
@@ -29,7 +29,44 @@ public class Huffman {
      */
     public File decompressFile(CompressedFile compressedFile)
     {
+        byte[] buffer = new byte[1024];
 
+        try {
+            File carpetaDestino = new File(directorioDestino);
+            if (!carpetaDestino.exists()) {
+                carpetaDestino.mkdir();
+            }
+
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(archivoComprimido));
+            ZipEntry entrada;
+
+            while ((entrada = zis.getNextEntry()) != null) {
+                String nombreArchivo = entrada.getName();
+                if (!entrada.isDirectory() && nombreArchivo.endsWith(".compr")) {
+                    String archivoDescomprimido = directorioDestino + File.separator + obtenerNombreSinExtension(nombreArchivo) + ".txt";
+                    File nuevoArchivo = new File(archivoDescomprimido);
+
+                    // Crear directorios si es necesario
+                    nuevoArchivo.getParentFile().mkdirs();
+
+                    // Extraer el archivo
+                    FileOutputStream fos = new FileOutputStream(nuevoArchivo);
+                    int longitud;
+                    while ((longitud = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, longitud);
+                    }
+                    fos.close();
+                }
+                zis.closeEntry();
+            }
+
+            zis.close();
+
+            System.out.println("¡Archivo descomprimido exitosamente!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
         return null;
     }
 
@@ -119,48 +156,12 @@ public class Huffman {
         }
     }
 
-    public static void descomprimirArchivo(String archivoComprimido, String directorioDestino) {
-        byte[] buffer = new byte[1024];
-
-        try {
-            File carpetaDestino = new File(directorioDestino);
-            if (!carpetaDestino.exists()) {
-                carpetaDestino.mkdir();
-            }
-
-            ZipInputStream zis = new ZipInputStream(new FileInputStream(archivoComprimido));
-            ZipEntry entrada;
-
-            while ((entrada = zis.getNextEntry()) != null) {
-                String nombreArchivo = entrada.getName();
-                if (!entrada.isDirectory() && nombreArchivo.endsWith(".compr")) {
-                    String archivoDescomprimido = directorioDestino + File.separator + obtenerNombreSinExtension(nombreArchivo) + ".txt";
-                    File nuevoArchivo = new File(archivoDescomprimido);
-
-                    // Crear directorios si es necesario
-                    nuevoArchivo.getParentFile().mkdirs();
-
-                    // Extraer el archivo
-                    FileOutputStream fos = new FileOutputStream(nuevoArchivo);
-                    int longitud;
-                    while ((longitud = zis.read(buffer)) > 0) {
-                        fos.write(buffer, 0, longitud);
-                    }
-                    fos.close();
-                }
-                zis.closeEntry();
-            }
-
-            zis.close();
-
-            System.out.println("¡Archivo descomprimido exitosamente!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Método para obtener el nombre del archivo sin la extensión
-    public static String obtenerNombreSinExtension(String nombreArchivo) {
+    /**
+     * Method that given the name of a file with the extension, returns the name of the file without the extension
+     * @param nombreArchivo, the name of the archive with the extension
+     * @return the name of the archive without the extension
+     */
+    private static String obtenerNombreSinExtension(String nombreArchivo) {
         int indicePunto = nombreArchivo.lastIndexOf(".");
         if (indicePunto != -1) {
             return nombreArchivo.substring(0, indicePunto);
