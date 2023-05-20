5dbf1f82c51371adea2b633857147669542715e8
Santiago Yepes Mesa <88009310+Laughcheeta1@users.noreply.github.com>
2023-05-20 15:18:05 -0500

    Update FileViewerBackEnd.java

--- backEnd/FileViewerBackEnd.java
+++ backEnd/FileViewerBackEnd.java
@@ -1,18 +1,9 @@
 package backEnd;
 
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 /**
  * Creator: 
  * This class is the back end of the TextViewer program.
  */
-
 public class FileViewerBackEnd {
 
 }
